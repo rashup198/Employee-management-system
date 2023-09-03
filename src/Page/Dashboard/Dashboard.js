d775c20bebab26c2fc0a8d30211e593df356df22
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-03 16:48:26 +0530

    updated Dashboard.js

--- src/Page/Dashboard/Dashboard.js
+++ src/Page/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {employeesData} from '../../Data/Data'
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 import { useState } from 'react'
 import List from './List'
@@ -17,18 +17,41 @@ const [employees, setEmployees] = useState(employeesData);
 
 const handleEdit=(id)=>{
     console.log("the id is" , id);
+    const [employee] = employees.filter(employee => employee.id === id)
+    setSelectedEmployee(employees);
+    setIsEditing(true)
+  }
+
+const handleDelete = (id) => {
+  console.log("the delete id is", id);
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'You will not be able to recover this employee!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'No, keep it'
+  }).then((result) => {
+    if (result.value) { 
+      const updatedEmployees = employees.filter(employee => employee.id !== id);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Employee Deleted Successfully',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+
+      setEmployees(updatedEmployees);
+    }
+  });
 }
 
-const handleDelete=(id)=>{
-
-  console.log("the delete id is" , id);
-
-}
 
   return (
     <div className=''>
       
-      <div>
+      <div className=''>
         {/* list of employees */}
         {!isAdding && !isEditing && (
           <div>
@@ -64,10 +87,10 @@ const handleDelete=(id)=>{
           isEditing && (
             <div>
               <Edit
-              setIsEditing={setIsEditing}
-              selectedEmployee={selectedEmployee}
               employees={employees}
+              selectedEmployee={selectedEmployee}
               setEmployees={setEmployees}
+              setIsEditing={setIsEditing}
               />
             </div>
           )}
